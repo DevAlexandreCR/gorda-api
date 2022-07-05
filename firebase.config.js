787caa0800b855fc80b03a5d6444c08ceb4b4812
AlexandreCR <devalexandrecr@gmail.com>
2022-07-04 19:33:33 -0500

    feature: add firebase config

--- firebase.config.js
+++ firebase.config.js
@@ -1,10 +1,10 @@
 export default {
-    apiKey: "AIzaSyBEF9nEWRooJ8Ov5mQW0tUtBp95sn5y2E4",
-    authDomain: "gorda-driver.firebaseapp.com",
-    projectId: "gorda-driver",
-    storageBucket: "gorda-driver.appspot.com",
-    messagingSenderId: "412940684394",
-    appId: "1:412940684394:web:40f1a71786e91a53040e13",
-    measurementId: "G-7P3L39LYYR"
+    apiKey: process.env.FIREBASE_API_KEY ?? "",
+    authDomain: process.env.FIREBASE_AUTHDOMAIN ?? "gorda.local",
+    projectId: process.env.FIREBASE_PROJECT_ID ?? "gorda-local",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? "gorda-local",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_ID ?? "12345678900",
+    appId: process.env.FIREBASE_APP_ID ?? "1:412940684394:web:40f1a71786e91a53040e13",
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID ?? "G-858886884"
 }
 
