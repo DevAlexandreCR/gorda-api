2e10fb928840b8db5ec41e9629e1d1b602597077
AlexandreCR <devalexandrecr@gmail.com>
2022-07-10 15:27:09 -0500

    feature: create client if not exists

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
   AUTHENTICATION_EMULATOR_HOST: process.env.AUTHENTICATION_EMULATOR_HOST || 'http://localhost:9099',
   DATABASE_EMULATOR_HOST: process.env.DATABASE_EMULATOR_HOST || 'http://localhost:9000',
   STORAGE_EMULATOR_HOST: process.env.STORAGE_EMULATOR_HOST || 'http://localhost:9199',
+  DEFAULT_CLIENT_PHOTO_URL: process.env.DEFAULT_CLIENT_PHOTO_URL || 'http://localhost',
   PQR_NUMBER: process.env.PQR_NUMBER || '+573000000000',
   SENTRY_DSN: process.env.SENTRY_DSN || 'http://localhost'
 }
---
+++ src/Models/Client.ts
@@ -0,0 +1,8 @@
+import {ClientInterface} from '../Interfaces/ClientInterface'
+
+export default class Client implements ClientInterface {
+    public id: string
+    public name: string
+    public phone: string
+    public photoUrl: string
+}
