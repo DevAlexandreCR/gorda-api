b05dfbc57191cc7b5a74b702705b216e79e9a5e7
AlexandreCR <devalexandrecr@gmail.com>
2022-07-16 21:03:38 -0500

    fix: error at create session

--- src/Services/chatBot/ChatBot.ts
+++ src/Services/chatBot/ChatBot.ts
@@ -2,10 +2,10 @@ import {Client, Message} from 'whatsapp-web.js'
 import Session from '../../Models/Session'
 import SessionRepository from '../../Repositories/SessionRepository'
 import {ResponseContext} from './MessageStrategy/ResponseContext'
+import {SessionInterface} from '../../Interfaces/SessionInterface'
 
 export default class ChatBot {
   private readonly wpClient: Client
-  private session: Session
   private messageFrom: string
   private message: Message
   
@@ -16,12 +16,19 @@ export default class ChatBot {
   async processMessage(message: Message): Promise<void> {
     this.setMessage(message)
     this.setMessageFrom(message)
-    this.session = new Session(message.from)
-    const active = await this.isSessionActive()
-    if (!active) {
-      await this.createSession().catch(e => console.log(e.message))
+    let session = new Session(message.from)
+    let sessionDB = await SessionRepository.findSessionByChatId(this.message.from)
+    const active = await this.isSessionActive(sessionDB)
+    if (active) {
+      Object.assign(session, sessionDB)
+    } else {
+      session = await this.createSession(session)
     }
-    await this.validateStatusSession()
+    const status = session.status as keyof typeof ResponseContext.RESPONSES
+    const handler = ResponseContext.RESPONSES[status]
+    const response = new ResponseContext(handler)
+    await response.processMessage(session, message, this.wpClient)
+
   }
   
   setMessage(message: Message): void {
@@ -32,22 +39,12 @@ export default class ChatBot {
     this.messageFrom = message.from
   }
   
-  async validateStatusSession(): Promise<void> {
-    const status = this.session.status as keyof typeof ResponseContext.RESPONSES
-    const handler = ResponseContext.RESPONSES[status]
-    const response = new ResponseContext(handler)
-    await response.processMessage(this.session, this.message, this.wpClient)
+  async isSessionActive(session: SessionInterface|null): Promise<boolean> {
+    return session !== null && session.status !== Session.STATUS_COMPLETED
   }
   
-  async isSessionActive(): Promise<boolean> {
-    const session = await SessionRepository.findSessionByChatId(this.message.from)
-    if (session) {
-      Object.assign(this.session, session)
-    }
-    return session !== null && !this.session.isCompleted()
-  }
-  
-  async createSession(): Promise<void> {
-    await SessionRepository.create(this.session)
+  async createSession(session: Session): Promise<Session> {
+    const sessionDB = await SessionRepository.create(session)
+    return Object.assign(session, sessionDB)
   }
 }
