29832abae2db79df41bba11c24657429d71d000d
alexandrecr <devalexandrecr@gmail.com>
2024-05-09 15:21:14 -0500

    ref: regex

--- src/Services/chatBot/Placeholders.ts
+++ src/Services/chatBot/Placeholders.ts
@@ -1,16 +1,16 @@
 export enum Placeholders {
-  PLATE = '[[PLATE]]',
-  COLOR = '[[COLOR]]',
-  USERNAME = '[[USERNAME]]',
-  PQR_NUMBER = '[[PQR-NUMBER]]',
-  PLACE = '[[PLACE]]',
-  COMPANY = '[[COMPANY]]',
+  PLATE = 'PLATE',
+  COLOR = 'COLOR',
+  USERNAME = 'USERNAME',
+  PQR_NUMBER = 'PQR-NUMBER',
+  PLACE = 'PLACE',
+  COMPANY = 'COMPANY',
 }
 
 export function replacePlaceholders(input: string, placeholders: Map<Placeholders, string>): string {
   let result = input
   placeholders.forEach((value, key) => {
-    const regex = new RegExp('\'\\\\[\\\\[\' + key + \'\\\\]\\\\]\', \'g\'')
+    const regex = new RegExp('\\[\\[' + key + '\\]\\]', 'g')
     result = result.replace(regex, value)
   })
 
