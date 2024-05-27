45568d006dba949b0a7845f6697533ff66876e56
DevalexandreCR <devalexandrecr@gmail.com>
2024-05-26 20:14:09 -0500

    style: add line

--- src/Services/whatsapp/interfaces/WpChatInterface.ts
+++ src/Services/whatsapp/interfaces/WpChatInterface.ts
@@ -3,6 +3,8 @@ import { WpMessageInterface } from './WpMessageInterface'
 
 export interface WpChatInterface {
   sendMessage(message: string): Promise<WpMessageInterface>
+
   archive(): Promise<void>
+
   getContact(): Promise<WpContactInterface>
 }
