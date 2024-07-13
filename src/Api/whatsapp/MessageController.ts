f8eb1a5646198f88b90990286f642cae28d7c704
DevalexandreCR <devalexandrecr@gmail.com>
2024-07-12 20:26:16 -0500

    feat: add route to validate

--- src/Api/whatsapp/MessageController.ts
+++ src/Api/whatsapp/MessageController.ts
@@ -14,7 +14,7 @@ const store = Store.getInstance()
 controller.post('/whatsapp/webhook', async (req: Request, res: Response) => {
   const { body } = req
   const entries = body.entry
-  console.log('webhook post', body.toString())
+  console.table(entries)
   const responseMessages: Array<string> = ['ok']
   entries.forEach((entry: any) => {
     const changes = entry.changes
