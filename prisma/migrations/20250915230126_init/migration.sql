8cde3c487049f5d0297de80ad72a90e5589552a1
DevAlexandreCR <devalexandrecr@gmail.com>
2025-09-15 18:01:51 -0500

    feat: add initial migration and update Prisma schema with new models and relations

--- prisma/migrations/20250915230126_init/migration.sql
+++ prisma/migrations/20250915230126_init/migration.sql
@@ -17,10 +17,9 @@ CREATE TABLE "public"."branches" (
 CREATE TABLE "public"."cities" (
     "id" VARCHAR(50) NOT NULL,
     "name" VARCHAR(100) NOT NULL,
-    "lat" DOUBLE PRECISION NOT NULL,
-    "lng" DOUBLE PRECISION NOT NULL,
-    "percentage" INTEGER,
-    "polygon" geometry(Polygon,4326) NOT NULL,
+    "center" geometry(Point,4326) NOT NULL,
+    "percentage" DOUBLE PRECISION NOT NULL,
+    "polygon" geometry(Polygon,4326),
     "branch_id" VARCHAR(50) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -32,9 +31,7 @@ CREATE TABLE "public"."cities" (
 CREATE TABLE "public"."places" (
     "id" TEXT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
-    "lat" DOUBLE PRECISION NOT NULL,
-    "lng" DOUBLE PRECISION NOT NULL,
-    "key" VARCHAR(100) NOT NULL,
+    "location" geometry(Point,4326) NOT NULL,
     "city_id" VARCHAR(50) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -46,7 +43,10 @@ CREATE TABLE "public"."places" (
 CREATE INDEX "cities_polygon_idx" ON "public"."cities" USING GIST ("polygon");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "places_key_key" ON "public"."places"("key");
+CREATE INDEX "cities_center_idx" ON "public"."cities" USING GIST ("center");
+
+-- CreateIndex
+CREATE INDEX "places_location_idx" ON "public"."places" USING GIST ("location");
 
 -- AddForeignKey
 ALTER TABLE "public"."cities" ADD CONSTRAINT "cities_branch_id_fkey" FOREIGN KEY ("branch_id") REFERENCES "public"."branches"("id") ON DELETE CASCADE ON UPDATE CASCADE;
