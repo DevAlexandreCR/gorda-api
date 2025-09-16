4167c89e0115773c088916ab0355a57cd905727a
DevAlexandreCR <devalexandrecr@gmail.com>
2025-09-15 23:13:39 -0500

    feat: set global timezone to UTC and configure Sequelize to use UTC for database operations

--- src/Database/sequelize.ts
+++ src/Database/sequelize.ts
@@ -1,6 +1,9 @@
 import { Sequelize } from 'sequelize'
 const config = require('../../config')
 
+// Force process to use UTC timezone globally
+process.env.TZ = 'UTC'
+
 const env = config.NODE_ENV || 'development'
 
 const dbConfig = require('./Config/database.js')[env]
@@ -9,7 +12,8 @@ const sequelize = new Sequelize(dbConfig.url, {
   dialect: dbConfig.dialect,
   dialectOptions: dbConfig.dialectOptions,
   logging: dbConfig.logging,
-  pool: dbConfig.pool
+  pool: dbConfig.pool,
+  timezone: '+00:00' // Force UTC timezone for database operations
 })
 
 export default sequelize
