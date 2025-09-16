b568633f003e096a64d9a2cc686985ec147eabea
DevAlexandreCR <devalexandrecr@gmail.com>
2025-09-15 21:22:38 -0500

    Refactor database configuration: replace TypeScript file with JavaScript version
    - Added a new JavaScript configuration file for database settings (database.js).
    - Removed the existing TypeScript configuration file (database.ts).
    - Updated connection settings for development, test, and production environments.

---
+++ src/Database/Config/database.js
@@ -0,0 +1,43 @@
+const config = require('../../../config')
+
+module.exports = {
+  development: {
+    url: config.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false
+    },
+    logging: console.log,
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  },
+  test: {
+    url: config.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false
+    },
+    logging: false
+  },
+  production: {
+    url: config.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false,
+    pool: {
+      max: 20,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+}
