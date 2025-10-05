4f679dac5c63a1c2cbe538244444d2cf5a576f6f
DevAlexandreCR <devalexandrecr@gmail.com>
2025-10-05 05:18:47 -0500

    feat: enhance place suggestion handling in Created response

--- src/Services/chatBot/MessageStrategy/Responses/Created.ts
+++ src/Services/chatBot/MessageStrategy/Responses/Created.ts
@@ -12,6 +12,8 @@ import { WpContactInterface } from '../../../../Services/whatsapp/interfaces/WpC
 import { MessageHandler } from '../../ai/MessageHandler'
 import { GordaChatBot } from '../../ai/Services/GordaChatBot'
 import { SessionStatuses } from '../../../../Types/SessionStatuses'
+import { PlaceSuggestionHelper } from '../../PlaceSuggestionHelper'
+import { PlaceOption } from '../../../../Interfaces/PlaceOption'
 
 export class Created extends ResponseContract {
   public messageSupported: Array<string> = [
@@ -62,12 +64,63 @@ export class Created extends ResponseContract {
       const ia = new MessageHandler(new GordaChatBot())
       const response = await ia.handleMessage(message.msg, SessionStatuses.ASKING_FOR_PLACE)
       if (response.place) {
-        const place = await this.store.findPlaceByName(response.place)
-        if (place) {
-          await this.sendMessage(Messages.requestingService(place.name)).then(async () => {
+        const searchResult = await this.store.findPlacesWithSuggestions(response.place)
+
+        if (searchResult.place && searchResult.hasExactMatch) {
+          await this.sendMessage(Messages.requestingService(searchResult.place.name)).then(async () => {
             await this.session.setStatus(SessionStatuses.ASKING_FOR_COMMENT)
-            await this.session.setPlace(place)
+            await this.session.setPlace(searchResult.place!)
           })
+        } else if (searchResult.place && !searchResult.hasExactMatch) {
+          const wpClient = this.store.wpClients[this.session.wp_client_id]
+          const confirmationMessage = PlaceSuggestionHelper.createConfirmationMessage(
+            searchResult.place.name,
+            wpClient?.service,
+            { id: this.session.id }
+          )
+          await this.sendMessage(confirmationMessage).then(async () => {
+            await this.session.setStatus(SessionStatuses.CHOOSING_PLACE)
+
+            // Store candidate place as option 0 (special case for confirmation)
+            const placeOptions: PlaceOption[] = [
+              { option: 0, placeId: `confirm:${searchResult.place!.id}` }
+            ]
+
+            // Add suggestions as additional options if available
+            if (searchResult.suggestions && searchResult.suggestions.length > 0) {
+              searchResult.suggestions.forEach((suggestion, index) => {
+                placeOptions.push({ option: index + 1, placeId: suggestion.id })
+              })
+            }
+
+            await this.session.setPlaceOptions(placeOptions)
+          })
+        } else if (searchResult.suggestions.length > 0) {
+          const wpClient = this.store.wpClients[this.session.wp_client_id]
+          const suggestionMessage = PlaceSuggestionHelper.createSuggestionMessage(
+            searchResult.suggestions.map((suggestion, index) => ({
+              option: index + 1,
+              placeId: suggestion.id,
+              placeName: suggestion.name
+            })),
+            response.place,
+            wpClient?.service,
+            { id: this.session.id }
+          )
+          await this.sendMessage(suggestionMessage).then(async () => {
+            await this.session.setStatus(SessionStatuses.CHOOSING_PLACE)
+
+            // Store each suggestion as a separate PlaceOption
+            const placeOptions: PlaceOption[] = searchResult.suggestions.map((suggestion, index) => ({
+              option: index + 1,
+              placeId: suggestion.id
+            }))
+
+            await this.session.setPlaceOptions(placeOptions)
+          })
+        } else {
+          const msg = Messages.getSingleMessage(MessagesEnum.NO_LOCATION_NAME_FOUND)
+          await this.sendMessage(msg)
         }
       } else if (response.sessionStatus === SessionStatuses.SUPPORT) {
         await this.session.setStatus(SessionStatuses.SUPPORT)
