730d8bb9ca02a49dd28a53f99d50b6dea2341868
DevAlexandreCR <devalexandrecr@gmail.com>
2025-10-19 16:37:41 -0500

    refactor: simplify error handling in HomeController

--- src/Api/Controllers/Home/HomeController.ts
+++ src/Api/Controllers/Home/HomeController.ts
@@ -4,16 +4,8 @@ import path from 'path'
 const controller = Router()
 
 controller.get('/', async (req: Request, res: Response) => {
-  try {
-    const htmlPath = path.join(__dirname, '../../../views/index.html')
-    return res.sendFile(htmlPath)
-  } catch (error) {
-    console.error('Error serving HTML file:', error)
-    return res.status(500).json({
-      success: false,
-      message: 'Error serving HTML file',
-    })
-  }
+  const htmlPath = path.join(__dirname, '../../../views/index.html')
+  return res.sendFile(htmlPath)
 })
 
 export default controller
